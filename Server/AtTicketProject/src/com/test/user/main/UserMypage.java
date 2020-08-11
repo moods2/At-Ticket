5a24f9752e9c804b307e695bc000562b96f12ace
JeongWoo <jwoo0823@naver.com>
2020-08-11 15:51:07 +0900

    user mypage jstl

--- Server/AtTicketProject/src/com/test/user/main/UserMypage.java
+++ Server/AtTicketProject/src/com/test/user/main/UserMypage.java
@@ -1,6 +1,7 @@
 package com.test.user.main;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,11 +11,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+
 @WebServlet("/usermypage.do")
 public class UserMypage extends HttpServlet{
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		HttpSession session = req.getSession();
+		
+		UserDAO dao = new UserDAO();
+		ArrayList<CouponDTO> clist = dao.getCoupon((int)session.getAttribute("userseq"));
+		ArrayList<EventDTO> elist = dao.getEvent();
+
+		
+		req.setAttribute("clist", clist);
+		req.setAttribute("elist", elist);
 		
 		RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/views/user/mypage/usermypage.jsp");
 		dispatcher.forward(req, resp);
